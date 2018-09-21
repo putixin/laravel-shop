91d392698a9fa554d4dbc67bae99719736cc89cb
putixin <52putixin@gmail.com>
2018-09-21 08:54:11 +0000

    管理后台 - 订单详情

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,7 +29,7 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton('alipay', function () {
             $config               = config('pay.alipay');
             // $config['notify_url'] = route('payment.alipay.notify');
-            $config['notify_url'] = 'http://requestbin.fullcontact.com/ts8gttts';            
+            $config['notify_url'] = 'http://requestbin.leo108.com/1g87dkd1';            
             $config['return_url'] = route('payment.alipay.return');
             // 判断当前项目运行环境是否为线上环境
             if (app()->environment() !== 'production') {
