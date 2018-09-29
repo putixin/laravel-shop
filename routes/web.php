5499666597d0f8b2cb0f8e8bc20e99c40a6f9100
putixin <52putixin@gmail.com>
2018-09-29 23:26:41 +0000

    用户界面 - 检查优惠券

--- routes/web.php
+++ routes/web.php
@@ -40,6 +40,7 @@ Route::group(['middleware' => 'auth'], function() {
 		Route::post('orders/{order}/review', 'OrdersController@sendReview')->name('orders.review.store');
 		Route::post('orders/{order}/apply_refund', 'OrdersController@applyRefund')->name('orders.apply_refund');
 		Route::post('payment/wechat/refund_notify', 'PaymentController@wechatRefundNotify')->name('payment.wechat.refund_notify');
+		Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
 	});
 	// 结束
 });
