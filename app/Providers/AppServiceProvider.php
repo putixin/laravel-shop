82d7269f68fc7e45a1a107d09b36c8c2991b9163
putixin <52putixin@gmail.com>
2018-09-29 06:04:53 +0000

    管理后台 - 同意退款 - 微信支付退款

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,8 +28,8 @@ class AppServiceProvider extends ServiceProvider
         // 往服务容器中注入一个名为 alipay 的单例对象
         $this->app->singleton('alipay', function () {
             $config               = config('pay.alipay');
-            // $config['notify_url'] = route('payment.alipay.notify');
-            $config['notify_url'] = 'http://requestbin.leo108.com/1g87dkd1';            
+            $config['notify_url'] = route('payment.alipay.notify');
+            // $config['notify_url'] = 'http://requestbin.leo108.com/1g87dkd1';            
             $config['return_url'] = route('payment.alipay.return');
             // 判断当前项目运行环境是否为线上环境
             if (app()->environment() !== 'production') {
@@ -44,7 +44,8 @@ class AppServiceProvider extends ServiceProvider
         // 往服务容器中注入一个名为 wechat_pay 的单例对象
         $this->app->singleton('wechat_pay', function () {
             $config = config('pay.wechat');
-            $config['notify_url'] = 'http://requestbin.fulcontact.com/[替换成你自己的url]';
+            // $config['notify_url'] = 'http://requestbin.leo108.com/1g87dkd1';
+            $config['notify_url'] = route('payment.wechat.notify');
             if (app()->environment() !== 'production') {
                 $config['log']['level'] = Logger::DEBUG;
             } else {
