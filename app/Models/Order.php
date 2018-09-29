539cec972ec1357e6656adc94e303b252d51f579
putixin <52putixin@gmail.com>
2018-09-29 02:12:37 +0000

    后台管理 - 同意退款 - 支付宝退款

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Ramsey\Uuid\Uuid;
 
 class Order extends Model
 {
@@ -76,18 +77,18 @@ class Order extends Model
         });
     }
 
-     public function user()
-     {
-     	return $this->belongsTo(User::class);
-     }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 
-     public function items()
-     {
-     	return $this->hasMany(OrderItem::class);
-     }
+    public function items()
+    {
+    	return $this->hasMany(OrderItem::class);
+    }
 
-     public static function findAvailableNo()
-     {
+    public static function findAvailableNo()
+    {
      	// 订单流水号前缀
      	$prefix = date('YmdHis');
      	for ($i = 0; $i < 10; $i++) {
@@ -101,5 +102,16 @@ class Order extends Model
      	\Log::warning('find order no failed');
 
      	return false;
-     }
+    }
+
+    public static function getAvailableRefundNo()
+    {
+        do {
+            // Uuid类可以用来生成大概率不重复的字符串
+            $no = Uuid::uuid4()->getHex();
+            // 为了避免重复我们在生成之后在数据库中查询看看是否已经存在相同的退款订单号
+        } while (self::query()->where('refund_no', $no)->exists());
+
+        return $no;
+    }
 }
